7422c7d27bb4f75441cfd9a03886c5a91f728b28
timothy timbol <ttimbol08@gmail.com>
2021-07-25 15:25:59 +0400

    added project model

--- models/Project.js
+++ models/Project.js
@@ -1,5 +1,24 @@
 const mongoose = require('mongoose');
 
 const projectScheme = new mongoose.Scheme({
-    
+    title: {
+        type: String,
+        required: true
+    },
+    imgUrl: {
+        type: String,
+        required: true
+    },
+    websiteLink: {
+        type: String,
+        required: true
+    },
+    websiteDescription: {
+        type: String,
+        required: true
+    },
+    technology: {
+        type: String,
+        required: true
+    }
 });
