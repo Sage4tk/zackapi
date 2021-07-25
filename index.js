de22b6cada295ec0914466c99c8d46350599d92d
timothy timbol <ttimbol08@gmail.com>
2021-07-25 13:17:39 +0400

    initialiazed portfolio api

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const favicon = require('serve-favicon')
-const path = require('path')
+const favicon = require('serve-favicon');
+const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -13,6 +14,16 @@ app.use(express.json({ limit: "2MB "}));
 app.use(express.urlencoded({extended:false}));
 app.use(express.static("public"))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+//database
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true},
+    () => console.log('Connected to database')
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
 
 //import routes
 const home = require('./routes/home');
---
+++ models/Project.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+const projectScheme = new mongoose.Scheme({
+    
+});
