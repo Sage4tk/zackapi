8980f4260049616f73c0fc5e19983817106abe57
timothy timbol <ttimbol08@gmail.com>
2021-07-27 20:28:43 +0400

    added jwt auth

---
+++ middleware/authToken.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const authToken = (req, res, next) => {
+    const header = req.headers['authorization'];
+    const token = header && header.split(' ')[1];
+    if (token == null) return res.status(401).json({msg: "Unauthorized"});
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, user) => {
+        if (err) return res.status(401).json({msg: "Unauthorized"});
+        req.user = user;
+        next();
+    })
+}
+
+module.exports = authToken();
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -6,18 +6,26 @@ let router = express.Router();
 //import models
 const projectSchema = require('../models/Project');
 
+//middleware
+const authToken = require('../middleware/authToken');
+
 router.route('/')
-.get((req, res) => {
-    return res.json({msg: "all good"})
+.get(async (req, res) => {
+    try {
+        const data = await projectSchema.find();
+        return res.json(data)
+    } catch (err) {
+        return res.json({msg: "Something went wrong", status: "failed"});
+    }
 })
-.post(async (req, res) => {
+.post(authToken, async (req, res) => {
     try {
         const saveItem = new projectSchema({
-            title: "test",
-            imgUrl: "test",
-            websiteLink: "test",
-            websiteDescription: "test",
-            technology: "test"
+            title: req.body.title,
+            imgUrl: req.body.imgUrl,
+            websiteLink: req.body.websiteLink,
+            websiteDescription: req.body.websiteDescription,
+            technology:  req.body.technology
         })
 
         await saveItem.save();
@@ -26,6 +34,6 @@ router.route('/')
         console.log(err)
         return res.status(500).json({msg: "something went wrong"})
     }
-})
+});
 
 module.exports = router
